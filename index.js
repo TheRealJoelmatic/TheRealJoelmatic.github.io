3ff4ce0ca457d02f9a639f928ca7cd849b39ef97
Joelmatic <78729990+TheRealJoelmatic@users.noreply.github.com>
2023-10-17 18:55:58 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -206,7 +206,7 @@ function checkPage() {
   var currentURL = window.location.href;
   var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
-  if (screenWidth < 800 & (currentURL).includes('moblie.html'))
+  if (screenWidth < 800 )
   {
     window.location.href = 'https://joelmatic.com/moblie.html';
   }
