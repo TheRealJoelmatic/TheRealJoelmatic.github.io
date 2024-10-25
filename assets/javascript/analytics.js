0ce6e908963ca85e3abb5199067351bb52b8b21d
Joelmatic <78729990+TheRealJoelmatic@users.noreply.github.com>
2024-10-25 12:40:52 +0100

    Add files via upload

---
+++ assets/javascript/analytics.js
@@ -0,0 +1,7 @@
+!function(o,b,n,x,i,u,s){o.GoogleAnalyticsObject=n;o[n]||(o[n]=function(){
+(o[n].q=o[n].q||[]).push(arguments)});o[n].x=+new Date;u=b.createElement(x);
+s=b.getElementsByTagName(x)[0];u.src=i;s.parentNode.insertBefore(u,s)}
+(window,document,'ga','script','https://www.google-analytics.com/analytics.js');
+
+ga('create', 'UA-122247573-1', 'vavy.cf');
+ga('send', 'pageview');
